9032de4ecc0aef6c779c1d61d476e56027480a42
StevenRincon24 <stevenrincon60@gmail.com>
2026-09-24 19:37:44 -0500

    Cambio: Implementacion inscripciones API

--- src/server.ts
+++ src/server.ts
@@ -444,4 +444,5 @@ if (isMainModule(import.meta.url) || process.env['pm_id']) {
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
+export {app};
 export const reqHandler = createNodeRequestHandler(app);
